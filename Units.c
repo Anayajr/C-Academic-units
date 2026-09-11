97dc446d9b2da23d4596cd44423de6c810d5c901
Anaya jr💕 <anayadunford38@gmail.com>
2026-09-11 15:55:46 +0300

    Add files via upload

---
+++ Units.c
@@ -0,0 +1,24 @@
+//program to display units on offer
+/*
+
+Author: Dunford
+Reg Number: BCS-05-0074/2026
+Description: Units on Offer
+Date: 11/9/2026
+Version 1
+*/
+
+#include <stdio.h>  // scanf(); printf()
+
+int main(){
+
+	printf("Units Offered This Semester:\n");
+	printf("1. Introduction to Programming\n");
+	printf("2. Data Structures\n");
+	printf("3. Computer Networks\n");
+	printf("4. Database Systems\n");
+	printf("5. Software Engineering\n");
+	
+	return  0;
+	
+}	
